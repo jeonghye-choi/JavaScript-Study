37ba2fb43025c732c60c3958cf13bad43b28d8db
jeonghye-choi <ppllhm.0@gmail.com>
2020-07-12 22:22:53 +0900

    docs : 예외처리
    try catch finally

--- test.js
+++ test.js
@@ -1,25 +1,9 @@
-class Person {
-    //constructor
-    constructor (name) {
-        this.name = name;
-    }
-
-    //method
-    sayHello() {
-        console.log(`안녕 나는 ${this.name}야`);
-    }
+try{
+    console.log("try에 있는 문장");
 }
+catch{
 
-
-// 상속
-// Person이 갖고 있는 걸 다 가지면서 자기만의 상태와 동작을 갖고싶어
-class Student extends Person {
-    //constructor
-    constructor(name, school, major){
-
-    }
 }
+finally{
 
-
-const jeonghye = new Person('정혜')
-jeonghye.sayHello();
+}
